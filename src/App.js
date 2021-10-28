69520e3cc5e9ded48bdfd9f390314a505bc69539
dchernandez17 <dchernandez@DESKTOP-PN3H1QH.localdomain>
2021-10-28 18:30:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DAISY <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
